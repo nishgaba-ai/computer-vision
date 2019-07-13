72631202fbc8f29f09b9485910c3a61c6750639e
Nishchal Gaba <nishgaba.ai@gmail.com>
2019-07-13 23:27:00 +0530

    Added Documentation using mkdocs

--- src/loadImage.py
+++ src/loadImage.py
@@ -1,8 +1,25 @@
 import cv2
+from PIL import Image
+import numpy as np
 
-def as_cv2_bgr(img_path):
+
+def as_bgr(img_path):
     '''
-    Loads Imge as CV2 BGR format
+    Loads Imge as CV2 BGR format (numpy array as of OpenCV 4)
     '''
     img = cv2.imread(img_path)
     return img
+
+def as_rgb(img_path):
+    '''
+    Loads Image as RGB format using pillow
+
+    Return a numpy array
+    '''
+
+    # NOTE: As this uses pillow format, PNG is loaded with RGBA format by default
+    img = Image.open(img_path)
+    arr = np.array(img)
+    return arr
+
+
