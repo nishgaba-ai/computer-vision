8b392443056600e7b965a3d10fc918c617dad9d3
Nishchal Gaba <nishgaba.ai@gmail.com>
2019-07-14 14:14:02 +0530

    Version 0.0.5 released for pip

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='nishchal@unrealai.xyz',
     author='Nishchal Gaba',
     url='https://github.com/nishgaba-ai/computer-vision',
-    version='0.0.4',
+    version='0.0.5',
     description='Machine Learning utilities to make your Project Development Life easier',
     py_modules=["helloworld"],
     package_dir={'':'src'},
