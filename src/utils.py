14a1a47730a314a1b1ed7e94e903fcac292e5746
Nishchal Gaba <nishgaba.ai@gmail.com>
2019-07-15 22:50:33 +0530

    Added module to check if input String/Number is Palindrome

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,18 @@ def euclideanDistance(src, targ):
         return "Error while calculating the euclidean distances for the above data types"
 
 
+def isStringPalindrome(data):
+    '''
+        Checks if input string or its coverted string is a palindrome
+    '''
+    
+    try:
+        # Convert input type to string
+        data = str(data)
+        return True if string==string[::-1] else False
 
+    except:
+        print("Error while processing, please check the input is correct")
 
 
 
